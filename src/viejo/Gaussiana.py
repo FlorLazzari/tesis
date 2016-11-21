0cd8fa2cf918b718d7158a1020b5b40e7e88c008
FlorLazzari <flor.lazzari@hotmail.com>
2016-11-21 11:28:22 -0300

    Carpeta Nuevo deleted

--- src/viejo/Gaussiana.py
+++ src/viejo/Gaussiana.py
@@ -17,50 +17,54 @@ from cart2pol import cart2pol
 
 class Gaussiana(object):
 
-    def __init__(self,coordenadas,c_T,case,k_estrella,epsilon):
+    def __init__(self, case, k_estrella, epsilon):
         super(Gaussiana, self).__init__()
         # aca no sirve de nada porque le puse (object)
-        self.coordenadas = coordenadas
-        self.c_T = c_T
         self.case = case
         self.k_estrella = k_estrella
         self.epsilon = epsilon
         # aca las coordenadas nuevas estan pisando las "coordenadas de Metodo"
         # no entiendo si me sirve para algo la inheritance en este caso
-        self.exponente = None
-        self.gauss = None
-        self.deficit_dividido_U_inf = None
-        self.x_n = None
-        self.y_n = None
-        self.z_n = None
-        self.r = None
-        self.phi = None
-        self.c = None
 
-    def play(self):
-        self.coordenadas.normalizar(self.case)
-        self.x_n = self.coordenadas.x_n
-        self.y_n = self.coordenadas.y_n
-        self.z_n = self.coordenadas.z_n
+
+    def play_pol(self, coordenadas, c_T):
+        coordenadas.normalizar(self.case)
+        self.c_T = c_T
+        self.x_n = coordenadas.x_n
+        self.y_n = coordenadas.y_n
+        self.z_n = coordenadas.z_n
         self.r, self.phi = cart2pol(self.y_n, self.z_n)
         sigma_n = self.k_estrella * self.x_n + self.epsilon
         sigma_n_cuadrado = (sigma_n)**2
         r_cuadrado = self.r**2
-        # el problema esta en c = 0
-        #
-        # C = 1 - (1-(C_T/(8*sigma_n_cuadrado)))**(1/2)
-        #
-        self.c = 1 - (1-(self.c_T/(8*sigma_n_cuadrado)))**(0.5)
-        self.exponente = np.zeros((len(self.x_n),len(self.r)))
+        c = 1 - (1-(self.c_T/(8*sigma_n_cuadrado)))**(0.5)
+        exponente = np.zeros((len(self.x_n),len(self.r)))
         self.gauss = np.zeros((len(self.x_n),len(self.r)))
         self.deficit_dividido_U_inf = np.zeros((len(self.x_n),len(self.r)))
         for i in range (0,len(self.x_n)):
             for j in range (0,len(self.r)):
-                self.exponente[i,j] = -r_cuadrado[j] / (2 * sigma_n_cuadrado[i])
-                self.gauss[i,j] = exp(self.exponente[i,j])
-                self.deficit_dividido_U_inf[i,j] = self.c[i] * self.gauss[i,j]
+                exponente[i,j] = -r_cuadrado[j] / (2 * sigma_n_cuadrado[i])
+                self.gauss[i,j] = exp(exponente[i,j])
+                self.deficit_dividido_U_inf[i,j] = c[i] * self.gauss[i,j]
 
-
-
-# hay que chequear bien cuales de todos estos son los que necesitan el self.
-# (osea, los que hará falta usar en el futuro para hacer Figuras)
+    def play_cart(self, coordenadas, c_T):
+        coordenadas.normalizar(self.case)
+        self.c_T = c_T
+        self.x_n = coordenadas.x_n
+        self.y_n = coordenadas.y_n
+        self.z_n = coordenadas.z_n
+        sigma_n = self.k_estrella * self.x_n + self.epsilon
+        sigma_n_cuadrado = (sigma_n)**2
+        #r_cuadrado = self.y_n**2 + self.z_n**2
+        c = 1 - (1-(self.c_T/(8*sigma_n_cuadrado)))**(0.5)
+        r_cuadrado = np.zeros((len(self.y_n),len(self.z_n)))
+        exponente = np.zeros((len(self.x_n),len(self.y_n),len(self.z_n)))
+        self.gauss = np.zeros((len(self.x_n),len(self.y_n),len(self.z_n)))
+        self.deficit_dividido_U_inf = np.zeros((len(self.x_n),len(self.y_n),len(self.z_n)))
+        for i in range (0,len(self.x_n)):
+            for j in range (0,len(self.y_n)):
+                    for k in range (0,len(self.z_n)):
+                        r_cuadrado[j,k] = (self.y_n[j])**2 + (self.z_n[k])**2
+                        exponente[i,j,k] = -r_cuadrado[j,k] / (2 * sigma_n_cuadrado[i])
+                        self.gauss[i,j,k] = exp(exponente[i,j,k])
+                        self.deficit_dividido_U_inf[i,j,k] = c[i] * self.gauss[i,j,k]
--- src/viejo/prueba_total.py
+++ src/viejo/prueba_total.py
@@ -21,9 +21,9 @@ z = d_0 * z_n
 case_1 = Case(0.15,0.125,2.2,0.42,0.00003,0.07)
 coordenadas_1 = Coordenadas(x,y,z)
 
-modelo_1 = Gaussiana(coordenadas_1,0.5,case_1,0.2,0.25)
+modelo_1 = Gaussiana(case_1,0.2,0.25)
 
-modelo_1.play()
+modelo_1.play_pol(coordenadas_1,0.5)
 
 
 from fraccionar import fraccionar
@@ -43,12 +43,59 @@ x_y = { 'x_1': modelo_1.r, 'y_1': modelo_1.gauss[v_0,barrido_r],
         'x_4': modelo_1.r, 'y_4': modelo_1.gauss[v_3,barrido_r]}
 
 figura_1 = Figura(nombre,x_y,xLabel,yLabel,numero)
-figura_1.show_save()
+# figura_1.show_save()
 
 # el save de la figura no esta funcionando
-################################################################################
+
 
 from Contour import Contour
+
+a = modelo_1.deficit_dividido_U_inf.transpose()
+
+x_z_a = {'x_1': modelo_1.x_n, 'z_1': modelo_1.r, 'a_1': a}
+
+contour_1 = Contour("contour_1",x_z_a,"xLabel","yLabel")
+
+# contour_1.show()
+
+################################################################################
+
+modelo_1 = Gaussiana(case_1,0.2,0.25)
+
+x_n = np.arange(0,15)
+y_n = np.arange(0,10)
+z_n = np.arange(0,10)
+
+d_0 = 0.15
+
+x = d_0 * x_n
+y = d_0 * y_n
+z = d_0 * z_n
+
+
+case_1 = Case(0.15,0.125,2.2,0.42,0.00003,0.07)
+coordenadas_2 = Coordenadas(x,y,z)
+
+modelo_1.play_cart(coordenadas_2,0.5)
+
+from fraccionar import fraccionar
+from barrer import barrer
+
+barrido_y_n = barrer(modelo_1.y_n)
+v_0, v_1, v_2, v_3 = fraccionar(modelo_1.x_n)
+
+nombre = "testDrive"
+xLabel = 'r'
+yLabel = r'$\Delta U / \Delta U_{max} $'
+numero = 1
+
+x_y = { 'x_1': modelo_1.y_n, 'y_1': modelo_1.gauss[v_0,barrido_y_n,4]}
+
+figura_1 = Figura(nombre,x_y,xLabel,yLabel,numero)
+figura_1.show_save()
+
+
+# from Contour import Contour
 #
 # a = modelo_1.deficit_dividido_U_inf.transpose()
 #
@@ -57,25 +104,3 @@ from Contour import Contour
 # contour_1 = Contour("contour_1",x_z_a,"xLabel","yLabel")
 #
 # contour_1.show()
-
-#########
-
-
-import matplotlib.pyplot as plt # para hacer los gráficos
-
-
-fig = plt.figure()
-plt.xlabel(r'$ x / d_{0} $')
-plt.ylabel('r')
-plt.title('Gaussian Filled Contour Plot')
-cp = plt.contourf(modelo_1.x_n,modelo_1.r,modelo_1.deficit_dividido_U_inf.transpose())
-plt.colorbar(cp)
-plt.show()
-plt.figure()
-
-
-##########
-# print(modelo_1.gauss)
-# print(modelo_1.exponente)
-print(modelo_1.c)
-#print(modelo_1.deficit_dividido_U_inf)
