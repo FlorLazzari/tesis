51270628a6116366bfe860679d1804e65d91a912
FlorLazzari <flor.lazzari@hotmail.com>
2017-03-23 17:31:49 -0300

    comparacion_blind_test_vs_gaussiana.py tiene resultados de OpenFOAM de gonza

--- src/comparacion_blind_test_vs_gaussiana.py
+++ src/comparacion_blind_test_vs_gaussiana.py
@@ -12,45 +12,52 @@
 from Figura_Scatter import Figura_Scatter
 from measurements_distance_1 import y,deficit_dividido_U_inf_y,sigma_y,z,deficit_dividido_U_inf_z,sigma_z
 from blind_test_Gaussiana_distancia_1 import y_Gaussiana, deficit_dividido_U_inf_Gaussiana
+from OpenFOAM_Blind_Test_distance_1_gonza import y_n,deficit_dividido_U_inf_OpenFOAM
 
 
 x_y = {"x_1" : y, "y_1" : deficit_dividido_U_inf_y,
-       "x_2" : y_Gaussiana, "y_2" : deficit_dividido_U_inf_Gaussiana}
+       "x_2" : y_Gaussiana, "y_2" : deficit_dividido_U_inf_Gaussiana,
+       "x_3" : y_n, "y_3" : deficit_dividido_U_inf_OpenFOAM}
 nombre = "y/d vs deficit_dividido_U_inf x/d=1"
 xLabel = r'$y/d$'
 yLabel = r'$ \Delta U / U_{\infty}$'
 
 
-figura_prueba = Figura_Scatter(nombre,x_y,xLabel,yLabel,2)
+figura_prueba = Figura_Scatter(nombre,x_y,xLabel,yLabel,3)
 figura_prueba.xLim = [-2,2]
 figura_prueba.show()
 
 
 from measurements_distance_3 import y,deficit_dividido_U_inf_y,sigma_y,z,deficit_dividido_U_inf_z,sigma_z
 from blind_test_Gaussiana_distancia_3 import y_Gaussiana, deficit_dividido_U_inf_Gaussiana
+from OpenFOAM_Blind_Test_distance_3_gonza import y_n,deficit_dividido_U_inf_OpenFOAM
+
 
 x_y = {"x_1" : y, "y_1" : deficit_dividido_U_inf_y,
-       "x_2" : y_Gaussiana, "y_2" : deficit_dividido_U_inf_Gaussiana}
+       "x_2" : y_Gaussiana, "y_2" : deficit_dividido_U_inf_Gaussiana,
+       "x_3" : y_n, "y_3" : deficit_dividido_U_inf_OpenFOAM}
 nombre = "y/d vs deficit_dividido_U_inf x/d=3"
 xLabel = r'$y/d$'
 yLabel = r'$ \Delta U / U_{\infty}$'
 
 
-figura_prueba = Figura_Scatter(nombre,x_y,xLabel,yLabel,2)
+figura_prueba = Figura_Scatter(nombre,x_y,xLabel,yLabel,3)
 figura_prueba.xLim = [-2,2]
 figura_prueba.show()
 
 from measurements_distance_5 import y,deficit_dividido_U_inf_y,sigma_y,z,deficit_dividido_U_inf_z,sigma_z
 from blind_test_Gaussiana_distancia_5 import y_Gaussiana, deficit_dividido_U_inf_Gaussiana
+from OpenFOAM_Blind_Test_distance_5_gonza import y_n,deficit_dividido_U_inf_OpenFOAM
 
 x_y = {"x_1" : y, "y_1" : deficit_dividido_U_inf_y,
        "x_2" : y_Gaussiana, "y_2" : deficit_dividido_U_inf_Gaussiana}
+       "x_3" : y_n, "y_3" : deficit_dividido_U_inf_OpenFOAM}
 nombre = "y/d vs deficit_dividido_U_inf x/d=5"
 xLabel = r'$y/d$'
 yLabel = r'$ \Delta U / U_{\infty}$'
 
 
-figura_prueba = Figura_Scatter(nombre,x_y,xLabel,yLabel,2)
+figura_prueba = Figura_Scatter(nombre,x_y,xLabel,yLabel,3)
 figura_prueba.xLim = [-2,2]
 figura_prueba.show()
 
