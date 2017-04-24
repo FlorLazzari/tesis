b17031e2144952598d0bbdbb3e16182ce45dd357
FlorLazzari <flor.lazzari@hotmail.com>
2017-04-24 10:08:49 -0300

    borre carpeta figuras y calcule errores en comparacion_epsilon.py

--- src/comparacion_epsilon.py
+++ src/comparacion_epsilon.py
@@ -25,15 +25,25 @@ epsilon_figura_case_3 = 0.253
 epsilon_figura_case_4 = 0.272
 epsilon_figura_case_5 = 0.257
 
-print "CASE 1: \n equ 19 = %f \n equ 21 = %f \n figura = %f" % (epsilon_19[0], epsilon_21[1], epsilon_figura_case_1)
-print "CASE 2: \n equ 19 = %f \n equ 21 = %f \n figura = %f" % (epsilon_19[1], epsilon_21[1], epsilon_figura_case_2)
-print "CASE 3: \n equ 19 = %f \n equ 21 = %f \n figura = %f" % (epsilon_19[1], epsilon_21[1], epsilon_figura_case_3)
-print "CASE 4: \n equ 19 = %f \n equ 21 = %f \n figura = %f" % (epsilon_19[1], epsilon_21[1], epsilon_figura_case_4)
-print "CASE 5: \n equ 19 = %f \n equ 21 = %f \n figura = %f" % (epsilon_19[1], epsilon_21[1], epsilon_figura_case_5)
+
+error_19_case_1 = (epsilon_figura_case_1 - epsilon_19[0])/epsilon_figura_case_1
+error_21_case_1 = (epsilon_figura_case_1 - epsilon_21[0])/epsilon_figura_case_1
+
+error_19_case_2 = (epsilon_figura_case_2 - epsilon_19[1])/epsilon_figura_case_2
+error_21_case_2 = (epsilon_figura_case_2 - epsilon_21[1])/epsilon_figura_case_2
+
+error_19_case_3 = (epsilon_figura_case_3 - epsilon_19[1])/epsilon_figura_case_3
+error_21_case_3 = (epsilon_figura_case_3 - epsilon_21[1])/epsilon_figura_case_3
+
+error_19_case_4 = (epsilon_figura_case_4 - epsilon_19[1])/epsilon_figura_case_4
+error_21_case_4 = (epsilon_figura_case_4 - epsilon_21[1])/epsilon_figura_case_4
+
+error_19_case_5 = (epsilon_figura_case_5 - epsilon_19[1])/epsilon_figura_case_5
+error_21_case_5 = (epsilon_figura_case_5 - epsilon_21[1])/epsilon_figura_case_5
 
 
-#
-# print "epsilon_19 (case 2) =", epsilon_19[1], "epsilon__figura (case 1) =", epsilon_figura_case_1
-# print "epsilon_21 (case 1) =", epsilon_21[0], "epsilon__figura (case 1) =", epsilon_figura_case_1
-# print "epsilon_21 (case 2) =", epsilon_21[1], "epsilon__figura (case 1) =", epsilon_figura_case_1
-#
+print "CASE 1: \n equ 19 = %f \n equ 21 = %f \n figura = %f \n error 19 = %f \n error 21 = %f " % (epsilon_19[0], epsilon_21[0], epsilon_figura_case_1, error_19_case_1, error_21_case_1)
+print "CASE 2: \n equ 19 = %f \n equ 21 = %f \n figura = %f \n error 19 = %f \n error 21 = %f " % (epsilon_19[1], epsilon_21[1], epsilon_figura_case_2, error_19_case_2, error_21_case_2)
+print "CASE 3: \n equ 19 = %f \n equ 21 = %f \n figura = %f \n error 19 = %f \n error 21 = %f " % (epsilon_19[1], epsilon_21[1], epsilon_figura_case_3, error_19_case_3, error_21_case_3)
+print "CASE 4: \n equ 19 = %f \n equ 21 = %f \n figura = %f \n error 19 = %f \n error 21 = %f " % (epsilon_19[1], epsilon_21[1], epsilon_figura_case_4, error_19_case_4, error_21_case_4)
+print "CASE 5: \n equ 19 = %f \n equ 21 = %f \n figura = %f \n error 19 = %f \n error 21 = %f " % (epsilon_19[1], epsilon_21[1], epsilon_figura_case_5, error_19_case_5, error_21_case_5)
