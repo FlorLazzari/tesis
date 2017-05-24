74d78ec531aa57a534cfbaeae7afea8486cf8534
FlorLazzari <flor.lazzari@hotmail.com>
2017-05-24 17:47:05 -0300

    cambio de implementacion en Gaussiana_2.py, prueba en prueba_cambios.py

--- src/fitear_gaussiana.py
+++ src/fitear_gaussiana.py
@@ -9,18 +9,13 @@ def fitear_gaussiana(x,y):
     mean = sum(x*y)
     sigma = sum(y*(x - mean)**2)
 
-    # por que esto no funciona?!?
-
-    # def gauss_function(x, a, x0, sigma, c):
-    #     return ((a*np.exp(-(x-x0)**2/(2*sigma**2))) - c)
-
     # Define model function to be used to fit to the data above:
 
-    def gauss_function(x, a, x0, sigma):
-        return (a*np.exp(-(x-x0)**2/(2*sigma**2)))
+    def gauss_function(x, a, x0, sigma, offset):
+        return ((a*np.exp(-(x-x0)**2/(2*(sigma**2))))+offset)
 
     # p0 is the initial guess for the fitting coefficients (A, mu and sigma above)
-    popt, pcov = curve_fit(gauss_function, x, y, p0 = [1, mean, sigma]) #, 0.1])  el -0.1 lo puse a dedo del grafico
+    popt, pcov = curve_fit(gauss_function, x, y, p0 = [1, mean, sigma, -0.1]) #, 0.1])  el -0.1 lo puse a dedo del grafico
 
     # plot data
     plt.plot(x, y,'x')
---
+++ src/prueba_cambios.py
@@ -0,0 +1,63 @@
+# coding=utf-8
+
+import numpy as np
+
+from Case_2 import Case
+from Coordenadas import Coordenadas
+from Coordenadas_Norm import Coordenadas_Norm
+from Gaussiana_2 import Gaussiana
+
+################################################################################
+
+d_0 = 0.15
+z_h = 0.125
+U_hub = 2.2
+
+pto = 1.2
+
+case = Case(d_0,z_h,U_hub)
+coord = np.array((pto, pto, pto))
+
+k_estrella = 0.2
+epsilon = 0.268855463528
+
+gaussiana = Gaussiana(case,k_estrella,epsilon)
+
+c_T = 0.5
+
+deficit_dividido_U_inf = gaussiana.evalDeficitNorm(coord,c_T)
+
+print "gaussiana nueva = ", deficit_dividido_U_inf
+
+################################################################################
+
+from Gaussiana import Gaussiana
+from Case import Case
+from indexar import indexar
+
+x = np.arange(0,1.5,0.1)
+y = np.arange(0,1.5,0.1)
+z = np.arange(0,1.5,0.1)
+
+indice_x_1 = indexar(x, pto)
+indice_y_1 = indexar(y, pto)
+indice_z_1 = indexar(z, pto)
+
+c_T = 0.5
+z_0 = 0.00003
+I_0 = 0.07
+
+case = Case(d_0,z_h,U_hub,c_T,z_0,I_0)
+coordenadas = Coordenadas(x,y,z)
+
+modelo = Gaussiana(case,k_estrella,epsilon)
+
+
+
+modelo.play_cart(coordenadas,c_T)
+
+print "gaussiana vieja = ", modelo.deficit_dividido_U_inf[indice_x_1, indice_y_1, indice_z_1]
+
+
+# problemas! no obtengo los mismos resultados para las implementaciones
+# distintas unicamente para pto = 1
