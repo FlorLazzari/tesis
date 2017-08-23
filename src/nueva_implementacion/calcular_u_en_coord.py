7557c4018d065d6d79d520ed65a08efad7919327
FlorLazzari <flor.lazzari@hotmail.com>
2017-08-23 14:59:27 -0300

    cambios

--- src/nueva_implementacion/calcular_u_en_coord.py
+++ src/nueva_implementacion/calcular_u_en_coord.py
@@ -16,11 +16,9 @@ def calcular_u_en_coord(modelo, u_inf, coord, parque_de_turbinas):
     u_coord = u_inf
     turbinas_a_la_izquierda = parque_de_turbinas.turbinas_a_la_izquierda_de_una_coord(coord)
     deficit_normalizado_en_coord = []
-
-    # print turbinas_a_la_izquierda
-
+# el parque ya esta inicializado, el c_T de la primera turbina esta calculado
     index = 0
-
+# el for no deberia incluir a la primera turbina
     for turbina in turbinas_a_la_izquierda:
         turbina.generar_coord_random()
         # ahora calculo la contribucion de cada turbina a la izquierda en las coord_random
@@ -29,22 +27,25 @@ def calcular_u_en_coord(modelo, u_inf, coord, parque_de_turbinas):
         U = []
         U_adentro_disco = np.array([])
         count = 0
-        if ((coord_random[1]-self.coord[1])**2 + (coord_random[2]-self.coord[2])**2 < (self.d_0/2)**2):
-            U = calcular_U_en_pto(Modelo, U_inf, coord_turbina, n, coord_random)
-            U_adentro_disco = np.append(U_adentro_disco, U)
-            count = count + U**2
-        U_medio_disco = np.mean(U_adentro_disco)
-        print(U_medio_disco)
-        c_T_tab = self.c_T_tabulado(U_medio_disco)
-        volume = (self.d_0)**2
-        integral_U_cuadrado = (volume * count)/N
-        T_turbina = c_T_tab * integral_U_cuadrado   # lo dividi por (0.5 * rho) porque luego dividire por eso
-        T_disponible = (U_medio_disco)**2 * (np.pi*(self.d_0/2)**2)     # lo dividi por (0.5 * rho) porque luego multiplicare por eso
-        c_T = T_turbina / T_disponible
-        print ('c_T calculado:', c_T)
-        print ('c_T_tab:', c_T_tab)
+        for coord_random in coord_random_arreglo:
+            if ((coord_random[1]-self.coord[1])**2 + (coord_random[2]-self.coord[2])**2 < (self.d_0/2)**2):
+                deficit_normalizado_en_coord_random = modelo.evaluar_deficit_normalizado(turbina, coord_random)
+                u_coord = u_coord * (1 - deficit_normalizado_en_coord_random)
+                U = u_coord
 
-
+                U = calcular_U_en_pto(Modelo, U_inf, coord_turbina, n, coord_random)
+                U_adentro_disco = np.append(U_adentro_disco, U)
+                count = count + U**2
+            U_medio_disco = np.mean(U_adentro_disco)
+            print(U_medio_disco)
+            c_T_tab = self.c_T_tabulado(U_medio_disco)
+            volume = (self.d_0)**2
+            integral_U_cuadrado = (volume * count)/N
+            T_turbina = c_T_tab * integral_U_cuadrado   # lo dividi por (0.5 * rho) porque luego dividire por eso
+            T_disponible = (U_medio_disco)**2 * (np.pi*(self.d_0/2)**2)     # lo dividi por (0.5 * rho) porque luego multiplicare por eso
+            c_T = T_turbina / T_disponible
+            print ('c_T calculado:', c_T)
+            print ('c_T_tab:', c_T_tab)
 
         turbina.calcular_c_T(gaussiana, u_inf, coord_turbina, n)
         # u_coord = u_coord * (1 - deficit_normalizado_en_coord)
