da469870f197c69ca61793c1e4bab150d78c6963
FlorLazzari <flor.lazzari@hotmail.com>
2018-02-05 18:37:31 -0300

    cambie el orden en prueba_figura_6_Gaussiana.py

--- src/nueva_implementacion/prueba_figura_6_Gaussiana.py
+++ src/nueva_implementacion/prueba_figura_6_Gaussiana.py
@@ -15,7 +15,7 @@ gaussiana = Gaussiana()
 u_inf = U_inf()
 u_inf.coord_hub = 2.2
 u_inf.perfil = 'log'
-N = 10000
+N = 1000
 
 turbina_0 = Turbina_Paper(Coord(np.array([0,0,0.125])))
 # z_0 de la superficie
