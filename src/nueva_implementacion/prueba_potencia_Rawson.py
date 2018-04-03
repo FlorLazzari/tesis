7ca1882bffd6d3c1cba460e7c2d84d801c988f78
FlorLazzari <flor.lazzari@hotmail.com>
2018-04-03 17:36:03 -0300

    new file: prueba_potencia ... Rawson?

---
+++ src/nueva_implementacion/prueba_potencia_Rawson.py
@@ -0,0 +1,15 @@
+"""
+Para verificar los valores de potencia obtenidos (para el Parque Rawson?? o para
+que ejemplo?? Blind Test tiene potencia?? porque el capitulo del parque rawson
+viene despues del de potencia) comparamos los resultados de los modelos reducidos
+con: las mediciones in situ y las corridas de OpenFOAM.
+
+Los casos que estudiaremos seran:
+. una turbina?
+. dos turbinas alineadas?
+
+A continuacion se grafica:
+    1)
+
+
+"""
