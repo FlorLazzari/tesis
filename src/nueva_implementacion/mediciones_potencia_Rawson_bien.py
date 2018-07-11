31e88d1e1d41b93cb594fbd9154668ab01b82494
FlorLazzari <flor.lazzari@hotmail.com>
2018-07-11 14:28:54 -0300

    los graficos quedaron corridos

---
+++ src/nueva_implementacion/mediciones_potencia_Rawson_bien.py
@@ -0,0 +1,155 @@
+# coding=utf-8
+
+from __future__ import division
+import numpy as np
+from numpy import exp
+import matplotlib.pyplot as plt
+
+from Gaussiana import Gaussiana   # Gaussiana pertenece a la clase Modelo
+from Frandsen import Frandsen
+from Jensen import Jensen
+from Parque_de_turbinas import Parque_de_turbinas
+from Turbina_Rawson import Turbina_Rawson
+from Coord import Coord
+from Estela import Estela
+from U_inf import U_inf
+from calcular_u_en_coord import calcular_u_en_coord
+
+"""
+Para verificar los valores de potencia obtenidos (para el Parque Rawson?? o para
+que ejemplo?? Blind Test tiene potencia?? porque el capitulo del parque rawson
+viene despues del de potencia) comparamos los resultados de los modelos reducidos
+con: las mediciones in situ y las corridas de OpenFOAM.
+
+Blind Test: tiene curva de c_P pero me parece que no tiene sentido calcular la potencia.
+Parque Rawson: gonza tiene mediciones y resultados de OpenFOAM para 2 turbinas alineadas
+de potencia en funcion del angulo con el que incide el viento.
+
+Entonces los casos que estudiaremos seran:
+. una turbina de Rawson (como para validar los resultados de potencia)
+. dos turbinas de Rawson alineadas separadas por 5 diametros
+    * calcular la relacion entre la potencia de la turbina_1/turbina_0 para
+    distintas direcciones de viento, para esto hay que crear la matriz de rotacion
+. 5 turbinas alineadas de Barcelona? (gonza tiene datos medidos pero no tiene corrida de openFOAM)
+
+"""
+
+from scipy.stats import chisquare
+
+from scipy import interpolate
+def interpolar(x, y, nuevo_x):
+    tck = interpolate.splrep(x, y, s=0)
+    nuevo_y = interpolate.splev(nuevo_x, tck, der=0)
+    return nuevo_y
+
+gaussiana = Gaussiana()
+u_inf = U_inf()
+u_inf.coord_mast = 8
+u_inf.perfil = 'log'
+N = 300
+
+z_mast = 80
+
+# casos medidos (elegi unicamente estas mediciones ya que son las que se ven
+# menos interferidas por las otras turbinas):
+# 4.7D : entre turbina 7 y 8
+# 5.7D : entre turbina 8 y 9
+# arreglo_distancia = [4.7, 5.7]
+
+turbina_0 = Turbina_Rawson(Coord(np.array([0,0,80]))) # chequear altura del hub
+distancia = 4.7
+D = turbina_0.d_0
+turbina_1 = Turbina_Rawson(Coord(np.array([distancia*D,0,80]))) # chequear altura del hub
+# z_0 de la superficie
+z_0 = 0.1
+
+x_o = 8*D
+y_o = 0
+z_o = 80
+
+coord = Coord(np.array([x_o, y_o, z_o]))
+
+# 1)
+################################################################################
+# comparo OpenFOAM con modelo reducido
+# Dos turbinas de Rawson alineadas separadas por 4.7 diametros
+#    * calcular la relacion entre la potencia de la turbina_1/turbina_0 para
+#    distintas direcciones de viento, para esto hay que crear la matriz de rotacion
+
+import csv
+
+ratio_medido = []
+
+tur_down = 7
+tur_up = 8
+centro = 320
+
+medido = np.loadtxt('med_{}_{}_{}.csv'.format(tur_down, tur_up, centro), delimiter = ' ')
+
+angulos_medido = medido[0,290:351]
+ratio_medido = medido[1,290:351]
+std_medido = medido[2,290:351]
+
+precision_ang_medido = 1
+angulos_medido = angulos = np.arange(-30, 30 + precision_ang_medido, precision_ang_medido)
+
+precision_ang = 1
+angulos = np.arange(-30, 30 + precision_ang, precision_ang)
+
+iters_estadistica = 200
+
+metodo_array = ['linear', 'rss', 'largest']
+metodo_label = {'linear': 'Lineal', 'rss': 'Cuadratica', 'largest':'Dominante'}
+
+frandsen = Frandsen()
+jensen = Jensen()
+
+modelo_array = [jensen, frandsen, gaussiana]
+modelo_label = {'Jensen': 'Jensen', 'Frandsen': 'Frandsen', 'Gaussiana':'Gaussiana'}
+
+chi_array = []
+p_array = []
+
+for modelo_deficit in modelo_array:
+    for metodo_superposicion in metodo_array:
+        turbina_0 = Turbina_Rawson(Coord(np.array([0,0,80]))) # chequear altura del hub
+        turbina_1 = Turbina_Rawson(Coord(np.array([distancia*D,0,80]))) # chequear altura del hub
+        parque_de_turbinas = Parque_de_turbinas([turbina_0, turbina_1], z_0, z_mast)
+        parque_de_turbinas.rotar(-30)
+        array_ratio = np.zeros(iters_estadistica)
+        sigma_ratio = []
+        ratio = []
+        for theta in angulos:
+            for i in range(iters_estadistica):
+                data_prueba = calcular_u_en_coord(modelo_deficit, metodo_superposicion, coord, parque_de_turbinas, u_inf, N)
+                array_ratio[i] = turbina_1.potencia/turbina_0.potencia
+            ratio = np.append(ratio, np.mean(array_ratio))
+            sigma_ratio = np.append(sigma_ratio, np.std(array_ratio))
+            parque_de_turbinas.rotar(precision_ang)
+            # print theta
+
+        plt.figure(figsize=(10,10))
+        # plt.title('Cociente de potencias para dos turbinas separadas por {}D'.format(distancia))
+        plt.plot(angulos, ratio, label = u'Modelo analítico', linewidth=3)
+        plt.fill_between(angulos, ratio-sigma_ratio, ratio+sigma_ratio, alpha=0.3)
+        # plt.errorbar(angulos, ratio, yerr=sigma_ratio, marker='o', markersize=3, label='kdsjghkjng', zorder=0)
+        plt.xlabel(u'dirección[º]', fontsize=25)
+        plt.ylabel(r'$P_1 / P_0$', fontsize=30)
+        plt.plot(angulos_medido, ratio_medido, 'o', label = 'Mediciones', markersize=10)
+        plt.xlim(-30,30)
+        plt.ylim(0.2,1.2)
+        plt.xticks(fontsize=22)
+        plt.yticks(fontsize=22)
+        plt.grid()
+        plt.legend(fontsize=16, loc= 'upper right')
+        # ratio_interpolado_modelado = interpolar(angulos, ratio, dir_medido)
+        # chi, p = chisquare(ratio_interpolado_modelado, f_exp=ratio_medido)
+        chi, p = chisquare(ratio, f_exp=ratio_medido)
+        chi_array = np.append(chi_array, chi)
+        p_array = np.append(p_array, p)
+        plt.savefig('potencia_{}_{}_{}'.format(metodo_label[metodo_superposicion], modelo_label[type(modelo_deficit).__name__], str(int(distancia))), dpi=300)
+
+        print ('{}_{} = {}'.format(metodo_label[metodo_superposicion], modelo_label[type(modelo_deficit).__name__], chi))
+
+    # print 'chi_array = ',chi_array
+    # print 'p = ',p
--- turbinasRawson gonza 10 julio/deficitTurbinas.py
+++ turbinasRawson gonza 10 julio/deficitTurbinas.py
@@ -1,33 +1,38 @@
 # -*- coding: utf-8 -*-
 import pylab as plt
-import csv  
+import csv
 import numpy as np
 
+'''
+Tenemos:
+    - rawson10min.csv => son MEDICIONES
+    - resultados_...csv => OPEN FOAM
 
+'''
 
-#################################################################################  
+#################################################################################
 #---FUNCIONES
-    
+
 #-----ABRIR .CSV Y GUARDARLO EN UNA TABLA
 def abrirCSV (archivo):
-    f = open(archivo, 'r') 
+    f = open(archivo, 'r')
     tabla=[]
     reader = csv.reader(f)  # creates the reader object
-    
+
     for row in reader:   # iterates the rows of the file in orders
         tabla.append(row)    # prints each row
     f.close()      # closing
-    
+
     return (tabla)
 
 
 def rawsonDatos(archivo):
-    #------DATOS DE RAWSON CADA 10MIN  
+    #------DATOS DE RAWSON CADA 10MIN
     tabla=abrirCSV(archivo)
     print('dato cada 10 min: '+tabla[0][2]+'/'+tabla[0][1]+'/'+tabla[0][0]+', hora:'+tabla[0][3]+', min: '+tabla[0][4]+', hasta '+tabla[len(tabla)-1][2]+'/'+tabla[len(tabla)-1][1]+'/'+tabla[len(tabla)-1][0]+', hora:'+tabla[len(tabla)-1][3]+', min: '+tabla[len(tabla)-1][4]+' ')
-    
+
     #---FILTRO LOS DATOS QUE TIENE NAN
-    tablaOk=[] 
+    tablaOk=[]
     for i in range (len(tabla)):
         malo=False
         #---recolecto datos para la rosa de vientos (sin Nans)
@@ -35,15 +40,14 @@ def rawsonDatos(archivo):
             if (np.isnan(float(tabla[i][j]))):
                 malo=True
         if (malo==False):
-            tablaOk.append(tabla[i]) 
+            tablaOk.append(tabla[i])
     return(tablaOk)
 
 def deficitMedido(tablaOk,tur_down,tur_up,centro):
     ##------BUSCO LOS DEFICITS MEIDIDOS ENTRE DOS TURBINAS
     U=8
-    rangU=1 
-    
-    
+    rangU=1
+
     deff=[]
     deffOk=[]
     out=[]
@@ -53,18 +57,18 @@ def deficitMedido(tablaOk,tur_down,tur_up,centro):
         deff.append([])
         deffOk.append([])
         DirDeff.append(i)
-    
+
     for i in range(len(tablaOk)):
         if float(tablaOk[i][5]) > U-rangU and float(tablaOk[i][5]) < U+rangU:
             deff[int(float(tablaOk[i][7]))].append(  float(tablaOk[i][94+tur_down]) / float(tablaOk[i][94+tur_up])   )
-    
+
     deffMean=[]
     deffMax=[]
     deffMin=[]
     deffstdMin=[]
     deffstdMax=[]
     deffstd=[]
-    for i in range(len(deff)): 
+    for i in range(len(deff)):
         if len(deff[i]) == 0:
             print('direccion vacia'+str(i))
             deffMean.append(1)
@@ -80,104 +84,103 @@ def deficitMedido(tablaOk,tur_down,tur_up,centro):
             deffstd.append(np.std(deff[i]) )
             deffstdMin.append(deffMean[i]-np.std(deff[i]) )
             deffstdMax.append(deffMean[i]+np.std(deff[i]) )
-    
-    
-    #---filtro los datos con dos desvios estandar    
+
+
+    #---filtro los datos con dos desvios estandar
     for i in range(len(deff)):
         for j in range(len(deff[i])):
             if abs(deff[i][j]-deffMean[i]) > 2*deffstd[i]:
                 out[i].append(deff[i][j])
             else:
                 deffOk[i].append(deff[i][j])
-    
+
     #miro la cantida dde oUtlayers
     """
     outCont=[]
     for i in range(len(out)):
         outCont.append(len(out[i]))
-        
+
     plt.figure(1)
     plt.scatter(DirDeff,outCont)
     """
-                
+
     #calculo todo de nuevo
     deffMean=[]
     deffMax=[]
     deffMin=[]
     deffstd=[]
-    for i in range(len(deff)): 
+    for i in range(len(deff)):
         if len(deffOk[i])<2:
             deffMean.append(np.nan)
             deffMin.append(np.nan)
             deffMax.append(np.nan)
             deffstd.append(np.nan )
-        else: 
+        else:
             deffMean.append(np.mean(deffOk[i]))
             deffMin.append(min(deffOk[i]))
             deffMax.append(max(deffOk[i]))
             deffstd.append(np.std(deffOk[i]) )
-    
-    #adimensionalizo        
+
+    #adimensionalizo
     DirAd=[]
     for i in range(len(DirDeff)):
         DirAd.append(DirDeff[i]-centro)
-    
+
     #ploteo mediciones
-    plt.figure(2)     
+    plt.figure(2)
     plt.errorbar(DirAd,deffMean, yerr=deffstd,color='gray',marker='o',markersize=3,label= "Measurements",zorder=0)
     plt.ylim(0.2,1.3)
     plt.xlim(-25,25)
-    
-    return()
-  
-def deficitOpenfoam(tur_down,tur_up,centro,codigo,color,nombre):
+    return DirAd, deffMean, deffstd
+
+def deficitOpenfoam(tur_down,tur_up,centro,codigo,color,nombre, archivo):
     #------IMPORTO LAS POTENCIAS DE LAS DOS TURBINAS OPENFOAM
     tabla = abrirCSV (archivo)
     #saco el encabezado
     tabla=tabla[2:]
-      
+
     P_up=[]
     P_down=[]
     DirDeff_cfd=[]
     Uinf_up=[]
-    
+
     #para P7/P8, centro 320
-    if tur_up==8 and tur_down==7: 
+    if tur_up==8 and tur_down==7:
         for i in range(0,len(tabla),4):
             textoDir=tabla[i][0].split()
             DirDeff_cfd.append(float(textoDir[1]))
-        
-        
+
+
         for i in range(3,len(tabla),4):
             Uinf_up.append(float(tabla[i][9]))
-        
-        
+
+
         for i in range(3,len(tabla),4):
             P_up.append(float(tabla[i][10]))
         for i in range(2,len(tabla),4):
             P_down.append(float(tabla[i][10]))
         correccion=0
         #plote velocidad
-        plt.figure(3) 
+        plt.figure(3)
         plt.plot(DirDeff_cfd,Uinf_up,color,linewidth=2,markersize=3,label= "U upstream")
         plt.grid()
         plt.show()
-        
-        
+
+
     #para P8/P9, centro 70
-    if tur_up==9 and tur_down==8: 
+    if tur_up==9 and tur_down==8:
         for i in range(0,len(tabla),4):
             textoDir=tabla[i][0].split()
             DirDeff_cfd.append(float(textoDir[1]))
-        
+
         for i in range(3,len(tabla),4):
             P_up.append(float(tabla[i][10]))
         for i in range(2,len(tabla),4):
             P_down.append(float(tabla[i][10]))
         correccion=0
-    
+
     #para P6/P9, centro 25
-    if tur_up==9 and tur_down==6:   
+    if tur_up==9 and tur_down==6:
         for i in range(0,len(tabla),6):
             textoDir=tabla[i][0].split()
             DirDeff_cfd.append(float(textoDir[1]))
@@ -186,9 +189,9 @@ def deficitOpenfoam(tur_down,tur_up,centro,codigo,color,nombre):
         for i in range(2,len(tabla),6):
             P_down.append(float(tabla[i][10]))
         correccion=0
-        
+
     #para P7/P9, centro 22
-    if tur_up==9 and tur_down==7: 
+    if tur_up==9 and tur_down==7:
         for i in range(0,len(tabla),6):
             textoDir=tabla[i][0].split()
             DirDeff_cfd.append(float(textoDir[1]))
@@ -197,55 +200,56 @@ def deficitOpenfoam(tur_down,tur_up,centro,codigo,color,nombre):
         for i in range(3,len(tabla),6):
             P_down.append(float(tabla[i][10]))
         correccion=0
-    
-    
-    
-    
+
     #ploteo la potencia de cada turb
-    plt.figure(1) 
-    #plt.plot(DirDeff_cfd,P_up,color,linewidth=1,markersize=3,label= "Potencia turb"+str(tur_up)+", "+codigo)    
-    plt.plot(DirDeff_cfd,P_down,color,linewidth=2,markersize=3,label= "Potencia turb"+str(tur_down)+", "+codigo+' ('+nombre+')')  
+    plt.figure(1)
+    #plt.plot(DirDeff_cfd,P_up,color,linewidth=1,markersize=3,label= "Potencia turb"+str(tur_up)+", "+codigo)
+    plt.plot(DirDeff_cfd,P_down,color,linewidth=2,markersize=3,label= "Potencia turb"+str(tur_down)+", "+codigo+' ('+nombre+')')
     plt.legend(loc='lower right', frameon=False)
-    plt.xlabel('Direction [deg]')  
-    plt.ylabel('Power turbine')  
+    plt.xlabel('Direction [deg]')
+    plt.ylabel('Power turbine')
     plt.grid()
-    plt.savefig("power_tur"+str(tur_down)+"_"+str(tur_up)+".png", dpi = 300)    
-    plt.show() 
-    
-    #plote el decit  
+    plt.savefig("power_tur"+str(tur_down)+"_"+str(tur_up)+".png", dpi = 300)
+    plt.show()
+
+    #plote el decit
     deff_cfd=[]
     for i in range(len(P_up)):
         deff_cfd.append(P_down[i]/P_up[i])
-    
+
     DirDeff_cfdAd=[]
     for i in range(len(DirDeff_cfd)):
         DirDeff_cfdAd.append(DirDeff_cfd[i]-centro-correccion)
-       
-    plt.figure(2)     
+
+    plt.figure(2)
     plt.plot(DirDeff_cfdAd,deff_cfd,color,linewidth=1,markersize=5,label= 'Adaptive AD',zorder=1)
     plt.legend(loc='lower right', frameon=False)
-    plt.xlabel('Centered wind direction [deg]')  
-    plt.ylabel('P'+str(tur_down)+" / P"+str(tur_up))  
-    
-    
-#################################################################################    
+    plt.xlabel('Centered wind direction [deg]')
+    plt.ylabel('P'+str(tur_down)+" / P"+str(tur_up))
+    return DirDeff_cfdAd, deff_cfd
+
+#################################################################################
 #------ACCIONES
 
+import csv
+
 #importo los datos de rawson
-archivo='/home/gonzalo/GIOL/Rawson/turbinas/rawson10min.csv'
-#tablaOk = rawsonDatos(archivo)
+archivo='rawson10min.csv'
+tablaOk = rawsonDatos(archivo)
 
 #ploteo el deficit medido
-tur_down=6
-tur_up=9
-centro=28 #la direccion donde se produce el maximo deficil
-deficitMedido(tablaOk,tur_down,tur_up,centro)
+tur_down=7
+tur_up=8
+centro=320 #la direccion donde se produce el maximo deficil
+DirAd_Medido, deffMean_Medido, deffstd_Medido = deficitMedido(tablaOk,tur_down,tur_up,centro)
 
-#ploteo el deficit de openfoam
-codigos=['v2a','v2b','v2c','v3c']
-nombre=['tonto, Uniform T','BSC, uniform T','nuestro, uniform T','nuestro, not uniform T']
-color=['bv-','bv--','bv:','rv-']
+with open('med_{}_{}_{}.csv'.format(tur_down, tur_up, centro), 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=' ')
+    writer.writerow(DirAd_Medido)
+    writer.writerow(deffMean_Medido)
+    writer.writerow(deffstd_Medido)
 
+#ploteo el deficit de openfoamº
 
 codigos=['v3c']
 nombre=['nuestro, not uniform T']
@@ -253,16 +257,21 @@ color=['rv-']
 
 
 for c in range(len(codigos)):
-    archivo='/home/gonzalo/GIOL/Rawson/turbinas/resultados'+str(tur_down)+'_'+str(tur_up)+'_'+codigos[c]+'.csv'
-    deficitOpenfoam(tur_down,tur_up,centro,codigos[c],color[c],nombre[c])
-    
+    archivo='resultados'+str(tur_down)+'_'+str(tur_up)+'_'+codigos[c]+'.csv'
+    DirDeff_cfdAd, deff_cfd = deficitOpenfoam(tur_down,tur_up,centro,codigos[c],color[c],nombre[c], archivo)
+
+with open('cfd_{}_{}_{}.csv'.format(tur_down, tur_up, centro), 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=' ')
+    writer.writerow(DirDeff_cfdAd)
+    writer.writerow(deff_cfd)
+
 plt.figure(2)
 plt.grid()
 #plt.plot([-30,30],[1,1],'k')
-plt.savefig("deficit_tur"+str(tur_down)+"_"+str(tur_up)+".png", dpi = 300)    
-plt.show() 
+plt.savefig("deficit_tur"+str(tur_down)+"_"+str(tur_up)+".png", dpi = 300)
+plt.show()
 
 
 
-#################################################################################    
+#################################################################################
 print('---termino----')
